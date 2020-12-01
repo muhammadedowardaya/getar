7db301d6e4638a4cfdfdd43afce5fa0111bf546b
muhammadedowardaya <65476273+muhammadedowardaya@users.noreply.github.com>
2020-12-01 14:29:15 +0700

    upload file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// const canvas = document.querySelector("#myCanvas");
+// const ctx = canvas.getContext("2d");
+
+// ctx.beginPath();
+// ctx.moveTo(1, 1);
+// ctx.lineTo(5, 80);
+// ctx.stroke();
+function buatGetaran(pola) {
+	var getaran = window.navigator.vibrate(pola);
+
+	// tampilkan hasilnya ke console
+	// kalau true berarti browser mendukung untuk membuat getaran
+	// tapi kalau false browser tidak mendukung
+	console.log(getaran);
+}
